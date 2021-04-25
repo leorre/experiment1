9c320d316cf1e5c43436bd6c872c1609c0c675bb
leorre newman <leorrenew@gmail.com>
2021-04-25 13:54:22 +0300

    third heroku development

--- pages/choosing/choosing.py
+++ pages/choosing/choosing.py
@@ -336,8 +336,8 @@ def insertUserChoices():
     print("index: ", vacIndex)
     print("recomm: ", is_recomm)
 
-    query = """UPDATE "users" SET "chosenVacId" = '%s', "chosenVac" = '%s', "vacIndex" = '%s' WHERE "id" = '%s'"""\
-            % (chosen_vacation_id, chosen_vacation, vacIndex, session['code'])
+    query = """UPDATE "users" SET "chosenVacId" = '%s', "vacIndex" = '%s' WHERE "id" = '%s'"""\
+            % (chosen_vacation_id, vacIndex, session['code'])
     interact_db(query=query, query_type='commit')
 
     # query = """UPDATE "users" SET "chosenVacId" = '%s', "chosenVac" = '%s', "vacIndex" = '%s',
