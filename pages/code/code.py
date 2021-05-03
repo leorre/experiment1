927e31557d7723e101f494c0ec9b56a8d12e3c4a
leorre newman <leorrenew@gmail.com>
2021-05-04 01:15:53 +0300

    third heroku deployment

--- pages/code/code.py
+++ pages/code/code.py
@@ -13,21 +13,16 @@ def code2():
     curr_pc_code = query_result[0][0]
     curr_mobile_code = query_result[0][1]
 
-    curr_code = curr_mobile_code + 1  ########## ONLY MOBILE!!!!!!!
-    query = """UPDATE "curr_codes" SET "currMobileCode" = '%s' WHERE "currMobileCode" = '%s'""" % (
-    curr_mobile_code + 1, curr_mobile_code)
-    interact_db(query=query, query_type='commit')
-
-    # if curr_pc_code - 1000 > curr_mobile_code - 2000:  # chosen device: mobile #1000 and 2000 - for start points...
-    #     curr_code = curr_mobile_code + 1
-    #     query = """UPDATE "curr_codes" SET "currMobileCode" = '%s' WHERE "currMobileCode" = '%s'""" % (
-    #     curr_mobile_code + 1, curr_mobile_code)
-    #     interact_db(query=query, query_type='commit')
-    # else:  # chosen device: pc
-    #     curr_code = curr_pc_code + 1
-    #     query = """UPDATE "curr_codes" SET "currPcCode" = '%s' WHERE "currMobileCode" = '%s'""" % (
-    #     curr_pc_code + 1, curr_mobile_code)
-    #     interact_db(query=query, query_type='commit')
+    if curr_pc_code - 1000 > curr_mobile_code - 2000:  # chosen device: mobile #1000 and 2000 - for start points...
+        curr_code = curr_mobile_code + 1
+        query = """UPDATE "curr_codes" SET "currMobileCode" = '%s' WHERE "currMobileCode" = '%s'""" % (
+        curr_mobile_code + 1, curr_mobile_code)
+        interact_db(query=query, query_type='commit')
+    else:  # chosen device: pc
+        curr_code = curr_pc_code + 1
+        query = """UPDATE "curr_codes" SET "currPcCode" = '%s' WHERE "currMobileCode" = '%s'""" % (
+        curr_pc_code + 1, curr_mobile_code)
+        interact_db(query=query, query_type='commit')
     print(curr_code)
     device = "smartphone"
     if curr_code < 2000: #1000-1999 it's pc user
